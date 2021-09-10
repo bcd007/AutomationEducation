8dfc5e991794e29b3fdc714c4635e1f1161a4355
Bob Dillon <dillon_robert_c@network.lilly.com>
2021-09-10 09:59:44 -0400

    Updated parameter data and direct piping

--- ScriptParameters.ps1
+++ ScriptParameters.ps1
@@ -1,30 +1,25 @@
-# To have the ability to provide parameters to a cmdlet in a script from pipeline, we first must know if that cmddlet has parameters that can take pipeline input
-
-get-help get-service -Parameter * | select-object name,required,pipelineInput,position
-
 # non-pipeline method for searching by name
 get-service -Name WebClient
+get-service WebClient
+
+# To have the ability to provide parameters to a cmdlet in a script from pipeline, we first must know if that cmddlet has parameters that can take pipeline input
+# To do that, use Get-Help to see the available parameter information
+
+get-help get-service -Parameter * | select-object name,required,pipelineInput,position,@{Name="Type";Expression={$_.Type.Name}} | Format-Table -AutoSize
+
+# name              required pipelineInput                  position Type
+# ----              -------- -------------                  -------- ----
+# ComputerName      false    True (ByPropertyName)          named    System.String[]
+# DependentServices false    False                          named    System.Management.Automation.SwitchParameter
+# DisplayName       true     False                          named    System.String[]
+# Exclude           false    False                          named    System.String[]
+# Include           false    False                          named    System.String[]
+# InputObject       false    True (ByValue)                 named    System.ServiceProcess.ServiceController[]
+# Name              false    True (ByPropertyName, ByValue) 0        System.String[]
+# RequiredServices  false    False                          named    System.Management.Automation.SwitchParameter
 
 # Use PipeLine input for Name parameter
 "WebClient" | get-service
-
-# Why did the cmdlet apply "WebClient" as "Name" instead of <some other parameter>?  It's the Position indicator for the parameter, Name is Position 0:
-
-# When parameters are positional, the parameter name is optional. PowerShell associates unnamed parameter values with the function parameters according to the order or position of the unnamed parameter values in the function command.
-# When parameters are not positional (they are "named"), the parameter name (or an abbreviation or alias of the name) is required in the command.
-
-# name              required pipelineInput                  position
-# ----              -------- -------------                  --------
-# ComputerName      false    True (ByPropertyName)          named
-# DependentServices false    False                          named
-# DisplayName       true     False                          named
-# Exclude           false    False                          named
-# Include           false    False                          named
-# InputObject       false    True (ByValue)                 named
-# Name              false    True (ByPropertyName, ByValue) 0
-
-# Can we take that pipeline object and place it in a file of <some> type, and use that the provide a script with parameters?
-# First, take a look at what allows us to provide pipeline parameters to a script:
 
 [CmdletBinding()]
 param (
@@ -50,6 +45,7 @@ param (
 # CSV
 import-csv .\services.csv
 
+import-csv .\services.csv | get-service
 import-csv .\services.csv | .\Get-ServiceUsingParameterFile.ps1
 
 # JSON
@@ -57,5 +53,7 @@ import-csv .\services.csv | .\Get-ServiceUsingParameterFile.ps1
 (Get-Content -Raw ".\services.json")
 # Lets convert that from a string to a custom object so PowerShell can more better use it!
 (Get-Content -Raw ".\services.json") | ConvertFrom-Json
+
 # Put it all together...
+(Get-Content -Raw ".\services.json") | ConvertFrom-Json | get-service
 (Get-Content -Raw ".\services.json") | ConvertFrom-Json | .\Get-ServiceUsingParameterFile.ps1
