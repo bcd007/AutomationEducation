e8733463943963d49b1827b6c87094713c355eb8
Robert Dillon <bdillon007@gmail.com>
2025-01-23 09:47:49 -0500

    Delete Get-ServiceUsingParameterFile.ps1

--- Get-ServiceUsingParameterFile.ps1
+++
@@ -1,7 +0,0 @@
-[CmdletBinding()]
-param (
-    [parameter(ValueFromPipelineByPropertyName=$true,Mandatory=$true)][string]$Name,
-    [parameter(ValueFromPipelineByPropertyName=$true)][string]$computerName
-)
-
-get-service -Name $Name -ComputerName $computerName
